3ce0d4f0c8ac471c80e3ef2725d1bc3e00666f61
meadm <44445181+meadm@users.noreply.github.com>
2021-07-09 10:34:56 -0500

    Delete pull_conserved_WRE.sh

--- Finding and analyzing occurrences of the combined motif/pull_conserved_WRE.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while read LINE; do
-    ID=$(echo $LINE | cut -d ' ' -f1,1)
-    grep -w $ID /Users/MattMead/Documents/Work/Collaborations/Jae-Hyuk\ Yu/flavus_nidulans_fumigatus_wetA/Updates/6th\ Update_October\ 2017/10-6-17/new_NNN_tables/ALL_induced.txt >> fumi_conserved_WRE.txt
-    grep -w $ID /Users/MattMead/Documents/Work/Collaborations/Jae-Hyuk\ Yu/flavus_nidulans_fumigatus_wetA/Updates/6th\ Update_October\ 2017/10-6-17/new_NNN_tables/ALL_reduced.txt >> fumi_conserved_WRE.txt
-done < FIMO_fumi_genes_regulated.txt
